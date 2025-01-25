c71dccca397abb35804e3b6d3981bc7a057c6743
osamasayd <osamasayd141@gmail.com>
2025-01-26 01:40:26 +0300

    config login api

---
+++ src/app/api/login/route.js
@@ -0,0 +1,24 @@
+import prisma from '@/app/lib/prisma';
+import bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+export async function POST(req) {
+  const body = await req.json();
+  const { email, password } = body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return NextResponse.json({ message: "User not found." }, { status: 400 });
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    return NextResponse.json({ message: "Invalid password." }, { status: 401 });
+  }
+
+  // حذف كلمة المرور قبل إرسال الاستجابة
+  const { password: _, ...userWithoutPassword } = user;
+
+  // نجاح العملية
+  return NextResponse.json({ message: "Login successful", user: userWithoutPassword }, { status: 200 });
+}
