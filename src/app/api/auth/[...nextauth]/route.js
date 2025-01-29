fc569f2718ff085536decc41234eb78db36c161f
osamasayd <osamasayd141@gmail.com>
2025-01-30 01:29:05 +0300

    update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,4 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/app/lib/prisma";
 import { authOptions } from "@/app/lib/auth";
 
 const handler = NextAuth(authOptions);
