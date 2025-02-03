fcecdf7ef4db9593f00e034c2bd419516e1f2f91
osamasayd <osamasayd141@gmail.com>
2025-02-04 00:35:35 +0300

    update SessionProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SessionProvider from "@/components/SessionProvider";
+import SessionProvider from "@/app/providers/SessionProvider";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/auth";
 
