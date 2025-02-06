a1a85daa75397cc0d09b30d7910966c9802a7659
osamasayd <osamasayd141@gmail.com>
2025-02-07 02:22:36 +0300

    create handleSignOut

--- src/app/_components/Signout.jsx
+++ src/app/_components/Signout.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { signOut } from 'next-auth/react';
 import { FiLogOut } from "react-icons/fi";
 
+const handleSignOut = async () => {
+    try {
+        // تسجيل الخروج من NextAuth (حذف الجلسة من قاعدة البيانات وإزالة الكوكيز)
+        await signOut({ redirect: false });
+        console.log("Signed out successfully");
+    } catch (error) {
+        console.error("Error during signout:", error);
+    }
+};
 
 const Signout = () => {
     return (
         <div className="relative">
             <FiLogOut size={20} className="absolute left-3 top-4 text-currentColor dark:text-neutral-100" />
             <button
-                onClick={() => signOut()}
+                onClick={handleSignOut}
                 className="w-full right-0 text-center px-4 py-3 text-lg text-neutral-100 hover:bg-zinc-600 rounded-lg"
             >
                 Log out
