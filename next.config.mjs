9bf6bc59c7768b3d2071dbddf8483cb755911a01
osamasayd <osamasayd141@gmail.com>
2025-02-06 03:55:15 +0300

    add ui defaultAvatar domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'lh3.googleusercontent.com',
-          port: '',
-          pathname: '/**', // Allow all paths under this hostname
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: ["ui-avatars.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**", // Allow all paths under this hostname
+      },
+    ],
+  },
+};
+
+export default nextConfig;
