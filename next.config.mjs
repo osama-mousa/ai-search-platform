fd52c987496a2665585fb40d931978c21d7c8fd8
osamasayd <osamasayd141@gmail.com>
2025-02-07 00:37:23 +0300

    add hostname to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ui-avatars.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**", // Allow all paths under this hostname
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
         port: "",
         pathname: "/**", // Allow all paths under this hostname
       },
