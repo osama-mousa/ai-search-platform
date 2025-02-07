31cc5f33a723236a180aa0451bb871226690d0be
osamasayd <osamasayd141@gmail.com>
2025-02-07 08:38:16 +0300

    create footer

---
+++ src/app/_components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="w-full py-3 text-center text-sm text-gray-600 dark:text-gray-400 fixed bottom-0">
+                &copy; {new Date().getFullYear()} Chat Platform
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
