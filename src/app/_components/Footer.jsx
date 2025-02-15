f623a7e871ece1bd9b8ef2faed9520c209ed6498
osamasayd <osamasayd141@gmail.com>
2025-02-15 04:33:12 +0300

    update style

--- src/app/_components/Footer.jsx
+++ src/app/_components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <footer className="left-[calc(50%+8rem)] transform -translate-x-1/2 w-full py-3 text-center text-xs text-gray-600 dark:text-gray-400 fixed bottom-0">
+            <footer className="w-full justify-center py-3 text-center text-xs text-gray-600 dark:text-gray-400 fixed bottom-0">
                 &copy; {new Date().getFullYear()} Chat Platform
             </footer>
         </div>
